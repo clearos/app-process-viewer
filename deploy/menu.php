8af5c2fd551373c08a1ea7051e138eb9dab978e8
Peter Baldwin <pbaldwin@clearfoundation.com>
2011-03-15 04:33:06 +0000

    - Removed old menu configuration

--- deploy/menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$menu['/app/process_viewer']['title'] = menu('process_viewer', 'process_viewer_process_viewer');
-$menu['/app/process_viewer']['category'] = menu('menu', 'menu_category_system');
-$menu['/app/process_viewer']['subcategory'] = menu('menu', 'menu_subcategory_resources');
-
