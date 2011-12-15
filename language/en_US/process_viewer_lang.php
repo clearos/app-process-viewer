03dc115aa747084b2e5d6f9f13c2453f582bc670
Ben Chambers <bchambers@clearfoundation.com>
2011-12-15 19:43:03 +0000

    - Added description

--- language/en_US/process_viewer_lang.php
+++ language/en_US/process_viewer_lang.php
@@ -1,6 +1,6 @@
 <?php
 
 $lang['process_viewer_app_name'] = 'Process Viewer';
-$lang['process_viewer_app_description'] = 'Process Viewer description'; // FIXME
+$lang['process_viewer_app_description'] = 'Tabular display of all programs (processes) running on the server.  Specific information on each process is listed, including process ID (PID), running time, CPU and memory usage and owner.';
 $lang['process_viewer_processes'] = 'Processes';
 $lang['process_viewer_process'] = 'Process';
