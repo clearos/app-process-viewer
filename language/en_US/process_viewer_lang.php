f57d536e683a18dc77d820cf71db441e0463e007
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-04-19 00:33:54 +0000

    - Harmonized description

--- language/en_US/process_viewer_lang.php
+++ language/en_US/process_viewer_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['process_viewer_app_name'] = 'Process Viewer';
-$lang['process_viewer_app_description'] = 'Tabular display of all programs (processes) running on the server.  Specific information on each process is listed, including process ID (PID), running time, CPU and memory usage and owner.';
+$lang['process_viewer_app_description'] = 'The Process Viewer provides a tabular display of all processes running on the system.  Specific information on each process is listed, including process ID, running time, CPU and memory usage.';
 $lang['process_viewer_processes'] = 'Processes';
 $lang['process_viewer_process'] = 'Process';
 $lang['process_viewer_command'] = 'Command';
