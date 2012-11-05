ce65a7448351f684873baf597cc25b86991b0c62
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-05 19:58:56 +0000

    - Sorted language file

--- language/en_US/process_viewer_lang.php
+++ language/en_US/process_viewer_lang.php
@@ -1,13 +1,13 @@
 <?php
 
-$lang['process_viewer_app_name'] = 'Process Viewer';
 $lang['process_viewer_app_description'] = 'The Process Viewer provides a tabular display of all processes running on the system.  Specific information on each process is listed, including process ID, running time, CPU and memory usage.';
-$lang['process_viewer_processes'] = 'Processes';
-$lang['process_viewer_process'] = 'Process';
+$lang['process_viewer_app_name'] = 'Process Viewer';
 $lang['process_viewer_command'] = 'Command';
 $lang['process_viewer_cpu'] = 'CPU';
 $lang['process_viewer_id'] = 'ID';
 $lang['process_viewer_memory'] = 'Memory';
 $lang['process_viewer_owner'] = 'Owner';
+$lang['process_viewer_processes'] = 'Processes';
+$lang['process_viewer_process'] = 'Process';
 $lang['process_viewer_running'] = 'Running';
 $lang['process_viewer_size'] = 'Size';
