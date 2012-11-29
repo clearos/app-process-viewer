dccd1fe1ac72c60f2ae8b4dd0db69096a768ec77
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-11-29 20:45:01 +0000

    - Prepped for reports beta

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'process_viewer';
-$app['version'] = '1.4.2';
+$app['version'] = '1.4.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-process-viewer.spec
+++ packaging/app-process-viewer.spec
@@ -1,7 +1,7 @@
 
 Name: app-process-viewer
 Epoch: 1
-Version: 1.4.2
+Version: 1.4.6
 Release: 1%{dist}
 Summary: Process Viewer
 License: GPLv3
