eff090d509bf17c86dffa71b0e190d3eb0674493
pcbaldwin <pbaldwin@clearfoundation.com>
2014-09-09 23:36:32 -0400

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'process_viewer';
-$app['version'] = '1.6.5';
+$app['version'] = '2.0.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-process-viewer.spec
+++ packaging/app-process-viewer.spec
@@ -1,7 +1,7 @@
 
 Name: app-process-viewer
 Epoch: 1
-Version: 1.6.5
+Version: 2.0.0
 Release: 1%{dist}
 Summary: Process Viewer
 License: GPLv3
