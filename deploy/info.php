00e30b3ad23430b6464b39070b72dd0d0dfb6f20
pcbaldwin <pbaldwin@clearfoundation.com>
2014-10-08 15:22:10 -0400

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'process_viewer';
-$app['version'] = '2.0.1';
+$app['version'] = '2.0.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
