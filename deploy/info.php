65d44b96ae678b5591603bed4ed2ea6749b5c617
pcbaldwin <pbaldwin@clearfoundation.com>
2014-11-13 00:13:40 -0500

    Prepping for 6.6.0 Beta 2

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'process_viewer';
-$app['version'] = '1.6.5';
+$app['version'] = '1.6.7';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
