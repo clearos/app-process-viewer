159d6f59db963c1a5b220811b5fd0645b969c214
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:00:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'The Usoro onye nkiri na-enye a tabular ngosi nke ihe nile Filiks na-agba ọsọ na usoro. Na ụfọdụ ozi na onye ọ bụla usoro edepụtara, gụnyere usoro ID, na-agba ọsọ oge, CPU na ebe nchekwa ojiji.';
+$lang['process_viewer_app_name'] = 'usoro onye nkiri';
+$lang['process_viewer_command'] = 'iwu';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'na ebe nchekwa';
+$lang['process_viewer_owner'] = 'nwe';
+$lang['process_viewer_process_cpu_usage'] = 'Usoro CPU ojiji';
+$lang['process_viewer_processes'] = 'Filiks';
+$lang['process_viewer_process_memory_usage'] = 'Usoro Memory ojiji';
+$lang['process_viewer_process'] = 'usoro';
+$lang['process_viewer_running'] = '-agba ọsọ';
+$lang['process_viewer_size'] = 'size';
