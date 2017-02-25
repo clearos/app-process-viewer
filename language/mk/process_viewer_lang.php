82ebb0d1ecade3a7997f4fb19906ee6b2eed16ed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:02:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Прегледувач на процесот обезбедува табеларен приказ на сите процеси се извршува на системот. се наведени конкретни информации за секој процес, вклучувајќи го и процесот проект, трчање време, процесорот и употребата на меморијата.';
+$lang['process_viewer_app_name'] = 'процес Прегледувач';
+$lang['process_viewer_command'] = 'команда';
+$lang['process_viewer_cpu'] = 'процесорот';
+$lang['process_viewer_id'] = 'проект';
+$lang['process_viewer_memory'] = 'меморија';
+$lang['process_viewer_owner'] = 'сопственикот';
+$lang['process_viewer_process_cpu_usage'] = 'Процесот оптеретеноста на процесорот';
+$lang['process_viewer_processes'] = 'процеси';
+$lang['process_viewer_process_memory_usage'] = 'Користењенамеморија процес';
+$lang['process_viewer_process'] = 'процес';
+$lang['process_viewer_running'] = 'Вклучување на';
+$lang['process_viewer_size'] = 'големина';
