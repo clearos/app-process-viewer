f750183a621661d822980206480b83b46789cc7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:58:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = '进程查看器提供系统上运行的所有进程的表格显示。 对每个进程的具体信息列，包括进程ID，运行时间，CPU和内存使用情况。';
+$lang['process_viewer_app_name'] = '进程查看器';
+$lang['process_viewer_command'] = '命令';
+$lang['process_viewer_cpu'] = '中央处理器';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = '记忆';
+$lang['process_viewer_owner'] = '所有者';
+$lang['process_viewer_process_cpu_usage'] = '工艺CPU使用率';
+$lang['process_viewer_processes'] = '流程';
+$lang['process_viewer_process_memory_usage'] = '进程的内存使用';
+$lang['process_viewer_process'] = '处理';
+$lang['process_viewer_running'] = '运行';
+$lang['process_viewer_size'] = '尺寸';
