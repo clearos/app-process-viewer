b5ab531851b2101e4ca4a5ab40f4deee71fbd429
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:05:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'ක්රියාවලිය Viewer පද්ධතිය මත ධාවනය වන සියලු ක්රියාවලීන් tabular සංදර්ශකය සපයයි. එක් එක් ක්රියාපටිපාටිය පිළිබඳ නිශ්චිත තොරතුරු කාලය, CPU සහ මතකය භාවිතය ධාවනය, ක්රියාවලිය හැඳුනුම්පත ඇතුළු ලැයිස්තුගත කර ඇත.';
+$lang['process_viewer_app_name'] = 'ක්රියාවලිය Viewer';
+$lang['process_viewer_command'] = 'විධානය';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'හැඳුනුම්පත';
+$lang['process_viewer_memory'] = 'මතකය';
+$lang['process_viewer_owner'] = 'හිමිකරු';
+$lang['process_viewer_process_cpu_usage'] = 'ක්රියාවලිය CPU භාවිතාව';
+$lang['process_viewer_processes'] = 'ක්රියාවලි';
+$lang['process_viewer_process_memory_usage'] = 'ක්රියාවලිය මතකය භාවිතය';
+$lang['process_viewer_process'] = 'ක්රියාවලිය';
+$lang['process_viewer_running'] = 'ධාවන';
+$lang['process_viewer_size'] = 'තරම';
