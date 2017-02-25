0a8a03dd0eb0c588c059d21b294c394b80ee178b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:02:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Ny Process Viewer manome tabular fampisehoana ny dingana rehetra eo amin&#39;ny rafitra nihazakazaka. Manokana momba ny dingana tsirairay voatanisa, anisan&#39;izany ny dingana ID, mihazakazaka ny fotoana, sy ny fitadidiana fampiasana CPU.';
+$lang['process_viewer_app_name'] = 'Process Viewer';
+$lang['process_viewer_command'] = 'Command';
+$lang['process_viewer_cpu'] = 'Unité centrale';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Memory';
+$lang['process_viewer_owner'] = 'Owner';
+$lang['process_viewer_process_cpu_usage'] = 'Process CPU Usage';
+$lang['process_viewer_processes'] = 'dingana';
+$lang['process_viewer_process_memory_usage'] = 'Process Memory Usage';
+$lang['process_viewer_process'] = 'Process';
+$lang['process_viewer_running'] = 'Running';
+$lang['process_viewer_size'] = 'Size';
