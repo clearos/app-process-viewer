d6099e58b0658e9fcce5874cf5233e55bceeb7d4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:02:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Ostentantur omnia acta processus tabular Visum est processus currit ratio praebet. Speciei processus notitia quemque voluerunt etiam processus ID, cursu temporis usu et memoria Pentium.';
+$lang['process_viewer_app_name'] = 'Processus Results';
+$lang['process_viewer_command'] = 'Mandatum';
+$lang['process_viewer_cpu'] = 'cpu';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Memoria';
+$lang['process_viewer_owner'] = 'dominus';
+$lang['process_viewer_process_cpu_usage'] = 'CPU processus Usu';
+$lang['process_viewer_processes'] = 'processus';
+$lang['process_viewer_process_memory_usage'] = 'Memoria Usu processus,';
+$lang['process_viewer_process'] = 'process';
+$lang['process_viewer_running'] = 'cursor';
+$lang['process_viewer_size'] = 'Size';
