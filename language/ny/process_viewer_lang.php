0fb50ff49c4499c251df268ef783291143d34866
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:58:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Kupirira mmodzi imapereka tabular chiwonetsero cha njira zonse kuthamanga pa dongosolo. tsatanetsatane iliyonse ndondomeko zili kuphatikizapo ndondomeko ID, nthawi athamanga, CPU ndi wakagwiritsidwe pamtima.';
+$lang['process_viewer_app_name'] = 'ndondomeko mmodzi';
+$lang['process_viewer_command'] = 'lamulo';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Memory';
+$lang['process_viewer_owner'] = 'mwini';
+$lang['process_viewer_process_cpu_usage'] = 'Ndondomeko CPU zakagwiritsidwe';
+$lang['process_viewer_processes'] = 'njira';
+$lang['process_viewer_process_memory_usage'] = 'Ndondomeko Memory zakagwiritsidwe';
+$lang['process_viewer_process'] = 'njira';
+$lang['process_viewer_running'] = 'akuthamanga';
+$lang['process_viewer_size'] = 'kukula';
