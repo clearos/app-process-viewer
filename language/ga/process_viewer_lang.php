81a88bcb89a88758f6fe8e1da5df093d7613f00d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:00:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Soláthraíonn an Viewer Próiseas taispeáint táblacha de gach próiseas ag rith ar an gcóras. Tá faisnéis shonrach ar gach próiseas a liostaítear, lena n-áirítear Aitheantais próiseas, am ag rith, LAP agus úsáide cuimhne.';
+$lang['process_viewer_app_name'] = 'Amharcán próiseas';
+$lang['process_viewer_command'] = 'Ordú';
+$lang['process_viewer_cpu'] = 'LAP';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'cuimhne';
+$lang['process_viewer_owner'] = 'úinéir';
+$lang['process_viewer_process_cpu_usage'] = 'Próiseas Úsáid LAP';
+$lang['process_viewer_processes'] = 'próisis';
+$lang['process_viewer_process_memory_usage'] = 'Úsáid Próiseas Cuimhne';
+$lang['process_viewer_process'] = 'próiseas';
+$lang['process_viewer_running'] = 'Rith';
+$lang['process_viewer_size'] = 'méid';
