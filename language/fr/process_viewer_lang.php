92ef3bc4cb6a0be42917065763596014172af585
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:59:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Le Process Viewer fournit un affichage tabulaire de tous les processus en cours d&#39;exécution sur le système. Des informations spécifiques sur chaque processus est répertorié, y compris ID de processus, temps de fonctionnement, utilisation du processeur et de la mémoire.';
+$lang['process_viewer_app_name'] = 'Process Viewer';
+$lang['process_viewer_command'] = 'Commander';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Mémoire';
+$lang['process_viewer_owner'] = 'Propriétaire';
+$lang['process_viewer_process_cpu_usage'] = 'Processus CPU Utilisation';
+$lang['process_viewer_processes'] = 'processus';
+$lang['process_viewer_process_memory_usage'] = 'Mémoire du processus d&#39;utilisation';
+$lang['process_viewer_process'] = 'Processus';
+$lang['process_viewer_running'] = 'Fonctionnement';
+$lang['process_viewer_size'] = 'Taille';
