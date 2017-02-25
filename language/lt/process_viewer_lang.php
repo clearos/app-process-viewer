5e3e0e6847cbb9fcb67fadaf960d603ad8a43cde
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:02:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Procesas Viewer "suteikia lentelių rodyti visus procesus veikia sistemoje. Konkrečią informaciją apie kiekvieno proceso yra sąraše, įskaitant proceso ID, važiavimo laikas, CPU ir atminties naudojimas.';
+$lang['process_viewer_app_name'] = 'procesas žiūrovas';
+$lang['process_viewer_command'] = 'komanda';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'atmintis';
+$lang['process_viewer_owner'] = 'savininkas';
+$lang['process_viewer_process_cpu_usage'] = 'Procesas procesoriaus naudojimas';
+$lang['process_viewer_processes'] = 'procesai';
+$lang['process_viewer_process_memory_usage'] = 'Procesas Atminties naudojimas';
+$lang['process_viewer_process'] = 'procesas';
+$lang['process_viewer_running'] = 'veikia';
+$lang['process_viewer_size'] = 'dydis';
