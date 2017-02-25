c2c9f02ddbe6430b3bbf39e1bbb5e991e4426f06
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:01:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'ಪ್ರಕ್ರಿಯೆ ವೀಕ್ಷಕ ವ್ಯವಸ್ಥೆಯಲ್ಲಿ ಕಾರ್ಯನಿರ್ವಹಿಸುತ್ತಿರುವ ಎಲ್ಲಾ ಪ್ರಕ್ರಿಯೆಗಳ ಒಂದು ಕೋಷ್ಟಕ ಪ್ರದರ್ಶನ ಒದಗಿಸುತ್ತದೆ. ಪ್ರತಿ ಪ್ರಕ್ರಿಯೆಯಲ್ಲಿ ನಿರ್ದಿಷ್ಟ ಮಾಹಿತಿ ಪ್ರಗತಿ ID, ಚಾಲನೆಯ ಸಮಯ CPU ಮತ್ತು ಮೆಮೊರಿ ಬಳಕೆ ಸೇರಿದಂತೆ, ಪಟ್ಟಿ ಮಾಡಲಾಗಿದೆ.';
+$lang['process_viewer_app_name'] = 'ಪ್ರಕ್ರಿಯೆ ವೀಕ್ಷಕ';
+$lang['process_viewer_command'] = 'ಕಮಾಂಡ್';
+$lang['process_viewer_cpu'] = 'ಸಿಪಿಯು';
+$lang['process_viewer_id'] = 'ಐಡಿ';
+$lang['process_viewer_memory'] = 'ಮೆಮೊರಿ';
+$lang['process_viewer_owner'] = 'ಮಾಲೀಕ';
+$lang['process_viewer_process_cpu_usage'] = 'ಪ್ರಕ್ರಿಯೆ CPU ಬಳಕೆ';
+$lang['process_viewer_processes'] = 'ಕಾರ್ಯವಿಧಾನಗಳು';
+$lang['process_viewer_process_memory_usage'] = 'ಪ್ರಕ್ರಿಯೆ ಸ್ಮರಣೆ ಬಳಕೆ';
+$lang['process_viewer_process'] = 'ಪ್ರಕ್ರಿಯೆ';
+$lang['process_viewer_running'] = 'ರನ್ನಿಂಗ್';
+$lang['process_viewer_size'] = 'ಗಾತ್ರ';
