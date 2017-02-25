ec934528678dbf71e4313c4defb6fd46f66e43c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:07:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Các Viewer Quy trình cung cấp một màn hình hiển thị dạng bảng của tất cả các tiến trình đang chạy trên hệ thống. Thông tin cụ thể về mỗi quá trình được liệt kê, bao gồm cả quá trình ID, thời gian chạy, CPU và bộ nhớ sử dụng.';
+$lang['process_viewer_app_name'] = 'Viewer Process';
+$lang['process_viewer_command'] = 'Chỉ huy';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Ký ức';
+$lang['process_viewer_owner'] = 'Chủ nhân';
+$lang['process_viewer_process_cpu_usage'] = 'Quy trình sử dụng CPU';
+$lang['process_viewer_processes'] = 'Processes';
+$lang['process_viewer_process_memory_usage'] = 'Cách sử dụng Process Memory';
+$lang['process_viewer_process'] = 'Quá trình';
+$lang['process_viewer_running'] = 'Đang chạy';
+$lang['process_viewer_size'] = 'Kích thước';
