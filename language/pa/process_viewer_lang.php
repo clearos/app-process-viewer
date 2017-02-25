281ec21a323b769b69df313878be6c1c941796a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:04:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'ਕਾਰਵਾਈ ਦਰਸ਼ਕ ਸਿਸਟਮ ਤੇ ਚੱਲ ਸਾਰੇ ਕਾਰਜ ਦਾ ਇੱਕ ਸਦੱਤੇਪ੍ਰੀਮੀਅਮ ਡਿਸਪਲੇਅ ਪ੍ਰਦਾਨ ਕਰਦਾ ਹੈ. ਹਰੇਕ ਕਾਰਜ &#39;ਤੇ ਖਾਸ ਜਾਣਕਾਰੀ ਕਾਰਜ ID, ਚੱਲ ਵਾਰ, CPU ਅਤੇ ਮੈਮੋਰੀ ਦੀ ਵਰਤੋ ਵੀ ਸ਼ਾਮਲ ਸੂਚੀਬੱਧ ਹੈ,.';
+$lang['process_viewer_app_name'] = 'ਕਾਰਵਾਈ ਦਰਸ਼ਕ';
+$lang['process_viewer_command'] = 'ਹੁਕਮ';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID ਨੂੰ';
+$lang['process_viewer_memory'] = 'ਮੈਮੋਰੀ';
+$lang['process_viewer_owner'] = 'ਮਾਲਕ';
+$lang['process_viewer_process_cpu_usage'] = 'ਕਾਰਵਾਈ CPU ਉਪਯੋਗਤਾ';
+$lang['process_viewer_processes'] = 'ਕਾਰਜ';
+$lang['process_viewer_process_memory_usage'] = 'ਮੈਮੋਰੀ ਉਪਯੋਗਤਾ';
+$lang['process_viewer_process'] = 'ਕਾਰਵਾਈ';
+$lang['process_viewer_running'] = 'ਚੱਲ ਰਿਹਾ ਹੈ';
+$lang['process_viewer_size'] = 'ਆਕਾਰ';
