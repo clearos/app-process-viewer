60002fb4c7e41a5eceff074d4978acac8754fe83
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:59:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'પ્રક્રિયા દર્શક સિસ્ટમ પર ચાલી બધી પ્રક્રિયાઓ એક કોઠા પ્રદર્શન પૂરી પાડે છે. દરેક પ્રક્રિયા પર ચોક્કસ માહિતી પ્રક્રિયા ID, ચાલી રહેલ સમય, CPU અને મેમરી વપરાશ સમાવેશ થાય છે યાદી થયેલ છે.';
+$lang['process_viewer_app_name'] = 'પ્રક્રિયા દર્શક';
+$lang['process_viewer_command'] = 'આદેશ';
+$lang['process_viewer_cpu'] = 'સી.પી.યુ';
+$lang['process_viewer_id'] = 'આઈડી';
+$lang['process_viewer_memory'] = 'મેમરી';
+$lang['process_viewer_owner'] = 'માલિક';
+$lang['process_viewer_process_cpu_usage'] = 'પ્રક્રિયા CPU વપરાશ';
+$lang['process_viewer_processes'] = 'પ્રક્રિયાઓ';
+$lang['process_viewer_process_memory_usage'] = 'પ્રક્રિયા મેમરી વપરાશ';
+$lang['process_viewer_process'] = 'પ્રક્રિયા';
+$lang['process_viewer_running'] = 'ચાલી રહેલ';
+$lang['process_viewer_size'] = 'માપ';
