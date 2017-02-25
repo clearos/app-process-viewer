b96e5577f5198b6513a03d452a5db530a7c537d7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:59:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Το Viewer Διαδικασία παρέχει ένα πίνακα εμφάνισης όλων των διεργασιών που τρέχουν στο σύστημα. Ειδικές πληροφορίες για κάθε διαδικασία που περιλαμβάνεται στη λίστα, συμπεριλαμβανομένων ID διεργασίας, χρόνος λειτουργίας, CPU και χρήση μνήμης.';
+$lang['process_viewer_app_name'] = 'Viewer διαδικασία';
+$lang['process_viewer_command'] = 'Εντολή';
+$lang['process_viewer_cpu'] = 'ΕΠΕΞΕΡΓΑΣΤΗΣ';
+$lang['process_viewer_id'] = 'ταυτότητα';
+$lang['process_viewer_memory'] = 'Μνήμη';
+$lang['process_viewer_owner'] = 'Ιδιοκτήτης';
+$lang['process_viewer_process_cpu_usage'] = 'Διαδικασίας χρήση της CPU';
+$lang['process_viewer_processes'] = 'διεργασίες';
+$lang['process_viewer_process_memory_usage'] = 'Χρήση μνήμης διαδικασία';
+$lang['process_viewer_process'] = 'Επεξεργάζομαι, διαδικασία';
+$lang['process_viewer_running'] = 'Τρέξιμο';
+$lang['process_viewer_size'] = 'Μέγεθος';
