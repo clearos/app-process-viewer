4305f915ca2edc8e24476a32ed1fba5de2e961e0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:56:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Procesi Viewer ofron një ekran të sheshtë të gjitha proceset drejtimin në sistemin. informacione specifike për çdo proces është në listë, duke përfshirë edhe procesin e identifikimit, kohën drejtimin, CPU dhe përdorimin e kujtesës.';
+$lang['process_viewer_app_name'] = 'procesi Viewer';
+$lang['process_viewer_command'] = 'komandë';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'kujtim';
+$lang['process_viewer_owner'] = 'pronar';
+$lang['process_viewer_process_cpu_usage'] = 'Procesi i CPU Usage';
+$lang['process_viewer_processes'] = 'proceset';
+$lang['process_viewer_process_memory_usage'] = 'Procesi Memory Usage';
+$lang['process_viewer_process'] = 'proces';
+$lang['process_viewer_running'] = 'drejtimin';
+$lang['process_viewer_size'] = 'madhësi';
