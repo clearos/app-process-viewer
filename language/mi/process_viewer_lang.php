260c3d04e1f757977c06c2d01c1752d0e345b8b4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:02:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'whakarato te Viewer Tukanga he whakaatu ripa o tukanga katoa rere i runga i te pūnaha. e rārangi mōhiohio tauwhāiti i runga i ia tukanga te, tae atu tukanga ID, wa rere, PTM me te whakamahi mahara.';
+$lang['process_viewer_app_name'] = 'tukanga Viewer';
+$lang['process_viewer_command'] = 'Whakahaua';
+$lang['process_viewer_cpu'] = 'PTM';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Pūmahara';
+$lang['process_viewer_owner'] = 'Kaipupuri';
+$lang['process_viewer_process_cpu_usage'] = 'Tukanga Mahinga PTM';
+$lang['process_viewer_processes'] = 'tukanga';
+$lang['process_viewer_process_memory_usage'] = 'Mahinga Tukanga Pūmahara';
+$lang['process_viewer_process'] = 'tukanga';
+$lang['process_viewer_running'] = 'rere';
+$lang['process_viewer_size'] = 'rahi';
