455cf595b5d10cd6354d47539e8e6a0ab9be4880
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:01:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'The Process Viewer veitir tabular sýna öllum ferlum í gangi á kerfinu. Sérstakar upplýsingar um hvert ferli er skráð, þ.mt ferli ID, hlaupandi tími, CPU og minni notkun.';
+$lang['process_viewer_app_name'] = 'Process Viewer';
+$lang['process_viewer_command'] = 'Command';
+$lang['process_viewer_cpu'] = 'örgjörvi';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'minni';
+$lang['process_viewer_owner'] = 'eigandi';
+$lang['process_viewer_process_cpu_usage'] = 'Aðferð CPU Notkun';
+$lang['process_viewer_processes'] = 'Ferli';
+$lang['process_viewer_process_memory_usage'] = 'Aðferð Memory Usage';
+$lang['process_viewer_process'] = 'aðferð';
+$lang['process_viewer_running'] = 'Running';
+$lang['process_viewer_size'] = 'Size';
