a72b57eaab61107dc5a3f90f004f6c4e3f61944f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:00:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'A Process Viewer egy táblázatos megjelenítése minden folyamat fut a rendszer. Konkrét információ minden egyes folyamat szerepel, beleértve a folyamat azonosítóját, futási idő, a CPU és a memória használat.';
+$lang['process_viewer_app_name'] = 'Process Viewer';
+$lang['process_viewer_command'] = 'Parancs';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'memória';
+$lang['process_viewer_owner'] = 'Tulajdonos';
+$lang['process_viewer_process_cpu_usage'] = 'Folyamat CPU';
+$lang['process_viewer_processes'] = 'folyamatok';
+$lang['process_viewer_process_memory_usage'] = 'Folyamat memória használat';
+$lang['process_viewer_process'] = 'Folyamat';
+$lang['process_viewer_running'] = 'Futás';
+$lang['process_viewer_size'] = 'Méret';
