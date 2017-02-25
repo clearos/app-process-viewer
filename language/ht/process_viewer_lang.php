d240a04642c7ea28262f16563cab13191f3ea1f6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:00:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Viewer la Pwosesis bay yon ekspozisyon tabulaire nan tout pwosesis kouri sou sistèm nan. se espesifik enfòmasyon sou chak pwosesis ki nan lis, ki gen ladan pwosesis ID, kouri tan, CPU ak itilizasyon memwa.';
+$lang['process_viewer_app_name'] = 'Pwosesis Viewer';
+$lang['process_viewer_command'] = 'kòmandman';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'memwa';
+$lang['process_viewer_owner'] = 'Mèt';
+$lang['process_viewer_process_cpu_usage'] = 'Pwosesis CPU Itilizasyon';
+$lang['process_viewer_processes'] = 'Pwosesis';
+$lang['process_viewer_process_memory_usage'] = 'Itilizasyon memwa Pwosesis';
+$lang['process_viewer_process'] = 'Pwosesis';
+$lang['process_viewer_running'] = 'kouri';
+$lang['process_viewer_size'] = 'Kantite moun ki';
