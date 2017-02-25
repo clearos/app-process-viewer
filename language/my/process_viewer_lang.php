6bb26ef960b7f60be51daed111afa020d7beb33f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:57:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'အဆိုပါလုပ်ငန်းစဉ် Viewer ကိုစနစ်အပေါ်အပြေးလုပ်ငန်းစဉ်များအားလုံးတစ်ဦး tabular display ကိုပေးစွမ်းသည်။ အသီးအသီးလုပ်ငန်းစဉ်အပေါ်တိကျသောသတင်းအချက်အလက်များအချိန်, CPU နဲ့မှတ်ဉာဏ်အသုံးပြုမှု running, process ID အပါအဝင်စာရင်းဖြစ်ပါတယ်။';
+$lang['process_viewer_app_name'] = 'Process ကို Viewer ကို';
+$lang['process_viewer_command'] = 'အမိန့်';
+$lang['process_viewer_cpu'] = 'စီပီယူ';
+$lang['process_viewer_id'] = 'ID ကို';
+$lang['process_viewer_memory'] = 'မှတ်ဉာဏ်';
+$lang['process_viewer_owner'] = 'ပိုင်ဆိုင်သူ';
+$lang['process_viewer_process_cpu_usage'] = 'Process ကို CPU ကိုအသုံးပြုမှု';
+$lang['process_viewer_processes'] = 'လုပ်ငန်းစဉ်များ';
+$lang['process_viewer_process_memory_usage'] = 'Process ကိုသိုလှောင်သည့်အသုံးပြုမှု';
+$lang['process_viewer_process'] = 'ဖြစ်စဉ်';
+$lang['process_viewer_running'] = 'ပြေး';
+$lang['process_viewer_size'] = 'အရွယ်';
