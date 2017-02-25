9d95bc562ccefedd971c5c81679430b8a41930e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:01:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'កម្មវិធីមើលដំណើរការនេះបានផ្តល់នូវបង្ហាញជាតារាងនៃដំណើរការទាំងអស់ដែលកំពុងរត់នៅលើប្រព័ន្ធ។ ពជាក់លាក់ស្តីពីដំណើរការគ្នាត្រូវបានរាយក្នុងនោះរួមមានលេខសម្គាល់ដំណើរការពេលវេលាកំពុងរត់, ស៊ីភីយូនិងការប្រើប្រាស់ការចងចាំ។';
+$lang['process_viewer_app_name'] = 'កម្មវិធីមើលដំណើរការ';
+$lang['process_viewer_command'] = 'ពាក្យបញ្ជា';
+$lang['process_viewer_cpu'] = 'ស៊ីភីយូ';
+$lang['process_viewer_id'] = 'លេខសម្គាល់';
+$lang['process_viewer_memory'] = 'អង្គចងចាំ';
+$lang['process_viewer_owner'] = 'ម្ចាស់';
+$lang['process_viewer_process_cpu_usage'] = 'ការប្រើប្រាស់ស៊ីភីយូដំណើរការ';
+$lang['process_viewer_processes'] = 'ដំណើរការ';
+$lang['process_viewer_process_memory_usage'] = 'ការប្រើប្រាស់សតិដំណើរការ';
+$lang['process_viewer_process'] = 'ដំណើរការ';
+$lang['process_viewer_running'] = 'ការរត់';
+$lang['process_viewer_size'] = 'ទំហំ';
