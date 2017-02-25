5abc9556aa8fd6285a52485426d29858f0d77285
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:00:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'प्रक्रिया दर्शक सिस्टम पर चल रहे सभी प्रक्रियाओं की एक सारणीबद्ध प्रदर्शन प्रदान करता है। प्रत्येक प्रक्रिया पर विशेष जानकारी प्रक्रिया आईडी, समय चल रहा है, सीपीयू और स्मृति के उपयोग सहित सूचीबद्ध है।';
+$lang['process_viewer_app_name'] = 'प्रक्रिया दर्शक';
+$lang['process_viewer_command'] = 'आदेश';
+$lang['process_viewer_cpu'] = 'सी पी यू';
+$lang['process_viewer_id'] = 'आईडी';
+$lang['process_viewer_memory'] = 'याद';
+$lang['process_viewer_owner'] = 'मालिक';
+$lang['process_viewer_process_cpu_usage'] = 'प्रक्रिया CPU उपयोग';
+$lang['process_viewer_processes'] = 'प्रक्रियाओं';
+$lang['process_viewer_process_memory_usage'] = 'प्रक्रिया स्मृति उपयोग';
+$lang['process_viewer_process'] = 'प्रक्रिया';
+$lang['process_viewer_running'] = 'रनिंग';
+$lang['process_viewer_size'] = 'आकार';
