d0cf3465a3fbf2858f4283f1967c09df2d1d1f01
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:07:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Process Viewer tizimi ishlayotgan barcha jarayonlar bir jadval ekran beradi. Har bir jarayon haqida maxsus ma&#39;lumot jarayon ID, yugurish vaqtida, protsessor va xotira foydalanish, shu jumladan, keltirilgan.';
+$lang['process_viewer_app_name'] = 'Process Viewer';
+$lang['process_viewer_command'] = 'buyruq';
+$lang['process_viewer_cpu'] = 'Markaziy protsessor';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'xotira';
+$lang['process_viewer_owner'] = 'egasi';
+$lang['process_viewer_process_cpu_usage'] = 'Process CPU foydalanish';
+$lang['process_viewer_processes'] = 'jarayonlar';
+$lang['process_viewer_process_memory_usage'] = 'Process Xotira foydalanish';
+$lang['process_viewer_process'] = 'jarayon';
+$lang['process_viewer_running'] = 'Running';
+$lang['process_viewer_size'] = 'Size';
