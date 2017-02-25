447f85706240b4f0b19e9982af8c87b6d17899b3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:01:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Процесс Viewer жүйесі іске қосылған барлық процестердің кестелік көрінісін береді. Әрбір процесі туралы нақты ақпарат уақыт, орталық процессор және жад пайдалануды іске қосу, технологиялық ID қоса алғанда, тізімделеді.';
+$lang['process_viewer_app_name'] = 'Процесс Viewer';
+$lang['process_viewer_command'] = 'бұйрық';
+$lang['process_viewer_cpu'] = 'Орталық Есептеуіш Бөлім';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'жад';
+$lang['process_viewer_owner'] = 'ие';
+$lang['process_viewer_process_cpu_usage'] = 'Процесс CPU Usage';
+$lang['process_viewer_processes'] = 'процестер';
+$lang['process_viewer_process_memory_usage'] = 'Процесс Жадынжұмсау';
+$lang['process_viewer_process'] = 'процесс';
+$lang['process_viewer_running'] = 'Жүгіру';
+$lang['process_viewer_size'] = 'Өлшем';
