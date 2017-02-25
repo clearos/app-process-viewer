8e7f3e8adaa37980b263789ba4d06f6c7f926d04
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:56:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Գործընթացը հեռուստադիտողի ապահովում է աղյուսակային ցուցադրման բոլոր գործընթացների վազում է համակարգի. Կոնկրետ տեղեկություններ յուրաքանչյուր գործընթացի, որը գտնվում է, այդ թվում `գործընթացի ID, հոսող ժամանակի, պրոցեսորի եւ հիշողության օգտագործման.';
+$lang['process_viewer_app_name'] = 'Process հեռուստադիտողի';
+$lang['process_viewer_command'] = 'Հրաման';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'հիշողություն';
+$lang['process_viewer_owner'] = 'սեփականատեր';
+$lang['process_viewer_process_cpu_usage'] = 'Process CPU օգտագործման';
+$lang['process_viewer_processes'] = 'գործընթացները';
+$lang['process_viewer_process_memory_usage'] = 'Process Հիշողության Օգտագործում';
+$lang['process_viewer_process'] = 'պրոցես';
+$lang['process_viewer_running'] = 'Running';
+$lang['process_viewer_size'] = 'չափ';
