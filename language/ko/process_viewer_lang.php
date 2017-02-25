11c027d1381b56c74c584273bc9de413a91a303b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:02:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = '프로세스 뷰어 시스템에서 실행중인 모든 프로세스의 표 표시를 제공합니다. 각 과정에 대한 구체적인 정보는 프로세스 ID, 실행 시간, CPU 및 메모리 사용을 포함하여 나열됩니다.';
+$lang['process_viewer_app_name'] = '프로세스 뷰어';
+$lang['process_viewer_command'] = '명령';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = '신분증';
+$lang['process_viewer_memory'] = '기억';
+$lang['process_viewer_owner'] = '소유자';
+$lang['process_viewer_process_cpu_usage'] = '프로세스 CPU 사용';
+$lang['process_viewer_processes'] = '프로세스';
+$lang['process_viewer_process_memory_usage'] = '프로세스 메모리 사용';
+$lang['process_viewer_process'] = '방법';
+$lang['process_viewer_running'] = '달리는';
+$lang['process_viewer_size'] = '크기';
