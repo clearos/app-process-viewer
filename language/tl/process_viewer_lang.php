e2ee5a631bdebdb12cd408a5d027ccdffdde07dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:07:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Ang Proseso Viewer ay nagbibigay ng isang hugis ng mga talaan display ng lahat ng mga proseso na tumatakbo sa sistema. Tiyak na impormasyon sa bawat proseso ay nakalista, kabilang ang mga proseso ID, tumatakbo oras, CPU at memory paggamit.';
+$lang['process_viewer_app_name'] = 'proseso Viewer';
+$lang['process_viewer_command'] = 'utos';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'memorya';
+$lang['process_viewer_owner'] = 'may-ari';
+$lang['process_viewer_process_cpu_usage'] = 'Proseso CPU Usage';
+$lang['process_viewer_processes'] = 'Mga proseso';
+$lang['process_viewer_process_memory_usage'] = 'Paggamit Process Memory';
+$lang['process_viewer_process'] = 'paraan';
+$lang['process_viewer_running'] = 'tumatakbo';
+$lang['process_viewer_size'] = 'laki';
