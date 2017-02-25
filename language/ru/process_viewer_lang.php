c74f0dcc88c5501a09644a0f65dba45b9185201b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:04:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Процесс просмотра обеспечивает табличную отображение всех процессов, запущенных в системе. Конкретная информация о каждом процессе есть в списке, включая идентификатор процесса, время работы, центрального процессора и памяти.';
+$lang['process_viewer_app_name'] = 'Process Viewer';
+$lang['process_viewer_command'] = 'команда';
+$lang['process_viewer_cpu'] = 'ЦПУ';
+$lang['process_viewer_id'] = 'Я БЫ';
+$lang['process_viewer_memory'] = 'Память';
+$lang['process_viewer_owner'] = 'владелец';
+$lang['process_viewer_process_cpu_usage'] = 'Использование ЦП Процесс';
+$lang['process_viewer_processes'] = 'Процессы';
+$lang['process_viewer_process_memory_usage'] = 'Использование памяти процессов';
+$lang['process_viewer_process'] = 'Обработать';
+$lang['process_viewer_running'] = 'Бег';
+$lang['process_viewer_size'] = 'Размер';
