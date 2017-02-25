36e327f58315858d3a2dc52c681fa8498a8d659e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:00:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'The tsari Vidiyo na samar da wani tabular nuni dukan matakai a guje a kan tsarin. Specific bayanai a kan kowane tsari ne da aka jera, ciki har da tsarin ID, Gudun lokaci, CPU da memory cutarwa.';
+$lang['process_viewer_app_name'] = 'tsari Vidiyo';
+$lang['process_viewer_command'] = 'umurnin';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Memory';
+$lang['process_viewer_owner'] = 'Mai';
+$lang['process_viewer_process_cpu_usage'] = 'Tsari CPU Anfani';
+$lang['process_viewer_processes'] = 'tafiyar matakai';
+$lang['process_viewer_process_memory_usage'] = 'Tsari Memory Anfani';
+$lang['process_viewer_process'] = 'tsari';
+$lang['process_viewer_running'] = 'Running';
+$lang['process_viewer_size'] = 'size';
