1cf736fbec22a8641a13030b88625170a3fab18b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:01:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'プロセスビューアは、システム上で実行中のすべてのプロセスを表形式で表示しています。 各プロセスに関する具体的な情報は、プロセスID、実行時間、CPUとメモリの使用状況を含め、記載されています。';
+$lang['process_viewer_app_name'] = 'プロセスビューア';
+$lang['process_viewer_command'] = 'コマンド';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'メモリ';
+$lang['process_viewer_owner'] = 'オーナー';
+$lang['process_viewer_process_cpu_usage'] = 'プロセスのCPU使用率';
+$lang['process_viewer_processes'] = 'プロセス';
+$lang['process_viewer_process_memory_usage'] = 'プロセスのメモリ使用量';
+$lang['process_viewer_process'] = 'プロセス';
+$lang['process_viewer_running'] = 'ランニング';
+$lang['process_viewer_size'] = 'サイズ';
