1ed949529c14005be2c51dc1c4c1b24594f77145
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:03:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Het proces Viewer geeft tabel weergave van alle processen die op het systeem. Specifieke informatie over elk proces wordt vermeld, met inbegrip van proces-id, looptijd, CPU en geheugengebruik.';
+$lang['process_viewer_app_name'] = 'Process Viewer';
+$lang['process_viewer_command'] = 'Opdracht';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID kaart';
+$lang['process_viewer_memory'] = 'Geheugen';
+$lang['process_viewer_owner'] = 'Eigenaar';
+$lang['process_viewer_process_cpu_usage'] = 'Proces CPU-gebruik';
+$lang['process_viewer_processes'] = 'Processen';
+$lang['process_viewer_process_memory_usage'] = 'Proces Memory Usage';
+$lang['process_viewer_process'] = 'Werkwijze';
+$lang['process_viewer_running'] = 'hardlopen';
+$lang['process_viewer_size'] = 'Grootte';
