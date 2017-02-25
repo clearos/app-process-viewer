c7c954b675ed3af77f3a36954d8cc1a2173b5f57
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:06:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'செயல்முறை பார்வையாளர் கணினியில் இயங்கும் எல்லா செயல்களின் சீரமைப்புப் காட்சி அளிக்கிறது. ஒவ்வொரு செயலுக்கும் குறிப்பிட்ட தகவல் செயல்முறை ID, இயங்கும் நேரம், CPU மற்றும் நினைவக பயன்பாடு உட்பட, பட்டியலிடப்பட்டுள்ளது.';
+$lang['process_viewer_app_name'] = 'செயல்முறை பார்வையாளர்';
+$lang['process_viewer_command'] = 'கட்டளை';
+$lang['process_viewer_cpu'] = 'சிபியு';
+$lang['process_viewer_id'] = 'ஐடி';
+$lang['process_viewer_memory'] = 'நினைவகம்';
+$lang['process_viewer_owner'] = 'உரிமையாளர்';
+$lang['process_viewer_process_cpu_usage'] = 'செயல்முறை CPU பயன்பாடு';
+$lang['process_viewer_processes'] = 'செயல்முறைகள்';
+$lang['process_viewer_process_memory_usage'] = 'செயல்முறை நினைவக பயன்பாடு';
+$lang['process_viewer_process'] = 'செயல்முறை';
+$lang['process_viewer_running'] = 'இயங்கும்';
+$lang['process_viewer_size'] = 'அளவு';
