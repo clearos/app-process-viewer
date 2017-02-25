d784eccccc548cbe8730acef53c622f1b0ab393e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:58:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'El Visor de processos ofereix una exhibició tabular de tots els processos que s&#39;executen en el sistema. La informació específica sobre cada procés és a la llista, incloent la ID de procés, temps d&#39;execució, ús de CPU i memòria.';
+$lang['process_viewer_app_name'] = 'Visor de processos';
+$lang['process_viewer_command'] = 'Comandament';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'identificació';
+$lang['process_viewer_memory'] = 'memòria';
+$lang['process_viewer_owner'] = 'propietari';
+$lang['process_viewer_process_cpu_usage'] = 'Procés d&#39;ús de la CPU';
+$lang['process_viewer_processes'] = 'Processos';
+$lang['process_viewer_process_memory_usage'] = 'Ús de la memòria Procés';
+$lang['process_viewer_process'] = 'procés';
+$lang['process_viewer_running'] = 'córrer';
+$lang['process_viewer_size'] = 'mida';
