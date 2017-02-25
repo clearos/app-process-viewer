f6493701288492002cf8449de4f48f52aeb065d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:08:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Mae&#39;r Gwyliwr Broses yn darparu arddangosfa ar ffurf tabl o&#39;r holl brosesau yn rhedeg ar y system. gwybodaeth benodol am bob proses yn rhestredig, gan gynnwys proses ID, amser rhedeg, CPU a chof chynefod.';
+$lang['process_viewer_app_name'] = 'Gwyliwr broses';
+$lang['process_viewer_command'] = 'Ardal Reoli';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'cof';
+$lang['process_viewer_owner'] = 'perchennog';
+$lang['process_viewer_process_cpu_usage'] = 'Proses CPU Chynefod';
+$lang['process_viewer_processes'] = 'prosesau';
+$lang['process_viewer_process_memory_usage'] = 'Defnydd Cof Proses';
+$lang['process_viewer_process'] = 'proses';
+$lang['process_viewer_running'] = 'rhedeg';
+$lang['process_viewer_size'] = 'maint';
