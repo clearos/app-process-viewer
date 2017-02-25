4ae00b35037811553bf4668fa2bf9a5c278b26a1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:01:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'პროცესი Viewer უზრუნველყოფს tabular ჩვენება ყველა პროცესების გაშვებული სისტემაში. კონკრეტული ინფორმაციის თითოეული პროცესი არის ჩამოთვლილი, მათ შორის პროცესი ID, ქრონომეტრაჟი, CPU და მეხსიერების გამოყენება.';
+$lang['process_viewer_app_name'] = 'პროცესი Viewer';
+$lang['process_viewer_command'] = 'Command';
+$lang['process_viewer_cpu'] = 'პროცესორი';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Memory';
+$lang['process_viewer_owner'] = 'Owner';
+$lang['process_viewer_process_cpu_usage'] = 'პროცესი CPU გამოყენება';
+$lang['process_viewer_processes'] = 'პროცესები';
+$lang['process_viewer_process_memory_usage'] = 'პროცესი მეხსიერება გამოყენება';
+$lang['process_viewer_process'] = 'პროცესი';
+$lang['process_viewer_running'] = 'სირბილი';
+$lang['process_viewer_size'] = 'ზომა';
