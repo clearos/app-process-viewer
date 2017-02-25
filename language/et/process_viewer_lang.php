aa0b01d8a3825b24c5769dcab6b5f67c783c67f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:59:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Protsess Viewer annab tabelina kuvada kõigi protsesside töötab süsteem. Konkreetset teavet iga protsessi, sisaldades protsessi ID, sõiduaega, CPU ja mälu kasutust.';
+$lang['process_viewer_app_name'] = 'protsessi Viewer';
+$lang['process_viewer_command'] = 'käsk';
+$lang['process_viewer_cpu'] = 'Protsessor';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Mälu';
+$lang['process_viewer_owner'] = 'omanik';
+$lang['process_viewer_process_cpu_usage'] = 'Protsessi CPU Usage';
+$lang['process_viewer_processes'] = 'protsessid';
+$lang['process_viewer_process_memory_usage'] = 'Protsessi Memory Usage';
+$lang['process_viewer_process'] = 'protsess';
+$lang['process_viewer_running'] = 'Running';
+$lang['process_viewer_size'] = 'suurus';
