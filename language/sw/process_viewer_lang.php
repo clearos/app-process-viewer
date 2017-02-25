d5247b9fde964076fdecea1a977f06ed12183311
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:06:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Mchakato mtazamaji hutoa tabular maonyesho ya taratibu zote mbio juu ya mfumo. taarifa maalum juu ya kila hatua waliotajwa, ikiwa ni pamoja mchakato ID, wakati mbio, CPU na matumizi ya kumbukumbu.';
+$lang['process_viewer_app_name'] = 'mchakato Viewer';
+$lang['process_viewer_command'] = 'Amri';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'kumbukumbu';
+$lang['process_viewer_owner'] = 'mmiliki';
+$lang['process_viewer_process_cpu_usage'] = 'Mchakato CPU Matumizi';
+$lang['process_viewer_processes'] = 'michakato';
+$lang['process_viewer_process_memory_usage'] = 'Mchakato Kumbukumbu Matumizi';
+$lang['process_viewer_process'] = 'mchakato';
+$lang['process_viewer_running'] = 'Kimbia';
+$lang['process_viewer_size'] = 'ukubwa';
