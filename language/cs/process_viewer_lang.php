9e219385174095d8b64cc034d817aa2453e01bf9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:58:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Prohlížeč proces poskytuje tabulkové zobrazení všech běžících procesů v systému. Specifické informace o každém procesu je uvedena včetně ID procesu, běh času, využití CPU a paměti.';
+$lang['process_viewer_app_name'] = 'Process Viewer';
+$lang['process_viewer_command'] = 'Příkaz';
+$lang['process_viewer_cpu'] = 'procesor';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Paměť';
+$lang['process_viewer_owner'] = 'Majitel';
+$lang['process_viewer_process_cpu_usage'] = 'Proces Využití CPU';
+$lang['process_viewer_processes'] = 'procesy';
+$lang['process_viewer_process_memory_usage'] = 'Využití paměti procesu';
+$lang['process_viewer_process'] = 'Proces';
+$lang['process_viewer_running'] = 'Běh';
+$lang['process_viewer_size'] = 'Velikost';
