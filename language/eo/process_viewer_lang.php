d82ae93f1ecf84417d63cfe06319b27745b495e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:58:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'La Procezo Viewer provizas tabular kompilaĵo de ĉiuj procezoj kurante en la sistemo. Specifa informo sur ĉiu procezo estas listigita, Inkluzivanta procezon ID, rultempo, CPU kaj memoro uzado.';
+$lang['process_viewer_app_name'] = 'procezo Viewer';
+$lang['process_viewer_command'] = 'komando';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'memoro';
+$lang['process_viewer_owner'] = 'posedanto';
+$lang['process_viewer_process_cpu_usage'] = 'Procezo CPU Uzado';
+$lang['process_viewer_processes'] = 'procezoj';
+$lang['process_viewer_process_memory_usage'] = 'Procezo Memoro Uzado';
+$lang['process_viewer_process'] = 'Procedo';
+$lang['process_viewer_running'] = 'kurante';
+$lang['process_viewer_size'] = 'grandeco';
