7d78ee71df1bfdf23f288cad1137d9880efd526f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:04:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'بیننده فرآیند یک صفحه نمایش جدولی از تمام فرایندهای در حال اجرا بر روی سیستم را فراهم می کند. اطلاعات خاص در هر یک از مراحل ذکر شده است، از جمله روند ID، زمان در حال اجرا، CPU و استفاده از حافظه.';
+$lang['process_viewer_app_name'] = 'روند بیننده';
+$lang['process_viewer_command'] = 'فرمان';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'شناسه';
+$lang['process_viewer_memory'] = 'حافظه';
+$lang['process_viewer_owner'] = 'مالک';
+$lang['process_viewer_process_cpu_usage'] = 'روند CPU طریقه استفاده';
+$lang['process_viewer_processes'] = 'فرآیندهای';
+$lang['process_viewer_process_memory_usage'] = 'حافظه پروسه طریقه استفاده';
+$lang['process_viewer_process'] = 'روند';
+$lang['process_viewer_running'] = 'در حال اجرا';
+$lang['process_viewer_size'] = 'اندازه';
