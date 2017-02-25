fbfdd04f1a184942c4efa603c569f1d15b2f465f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:07:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Процес перегляду забезпечує табличную відображення всіх процесів, запущених в системі. Конкретна інформація про кожному процесі є в списку, включаючи ідентифікатор процесу, час роботи, центрального процесора і пам&#39;яті.';
+$lang['process_viewer_app_name'] = 'Process Viewer';
+$lang['process_viewer_command'] = 'команда';
+$lang['process_viewer_cpu'] = 'ЦП';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'пам&#39;ять';
+$lang['process_viewer_owner'] = 'власник';
+$lang['process_viewer_process_cpu_usage'] = 'Використання ЦП Процес';
+$lang['process_viewer_processes'] = 'процеси';
+$lang['process_viewer_process_memory_usage'] = 'Використання пам&#39;яті процесів';
+$lang['process_viewer_process'] = 'процес';
+$lang['process_viewer_running'] = 'біг';
+$lang['process_viewer_size'] = 'розмір';
