a748449693b7c1eb8cd04ccf79b2f974c23e0d56
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:06:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'ప్రాసెస్ వ్యూయర్ కంప్యూటరులో నడుస్తున్న అన్ని ప్రక్రియల పట్టిక ప్రదర్శన అందిస్తుంది. ప్రతి ప్రక్రియ నిర్దిష్ట సమాచారాన్ని ప్రాసెస్ ID, నడుస్తున్న సమయం, CPU మరియు మెమరీ వాడుక సహా, జాబితా ఉంది.';
+$lang['process_viewer_app_name'] = 'ప్రాసెస్ వ్యూయర్';
+$lang['process_viewer_command'] = 'కమాండ్';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'మెమరీ';
+$lang['process_viewer_owner'] = 'యజమాని';
+$lang['process_viewer_process_cpu_usage'] = 'ప్రాసెస్ CPU వినియోగం';
+$lang['process_viewer_processes'] = 'ప్రాసెసెస్';
+$lang['process_viewer_process_memory_usage'] = 'ప్రాసెస్ మెమరీ వాడుక';
+$lang['process_viewer_process'] = 'ప్రాసెస్';
+$lang['process_viewer_running'] = 'రన్నింగ్';
+$lang['process_viewer_size'] = 'పరిమాణం';
