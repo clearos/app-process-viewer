e2d9f04c8a4a0f521aab6595a1185e3391412220
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:03:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Il Viewer Proċess jipprovdi wirja tabulari tal-proċessi kollha li jimxi fuq is-sistema. Informazzjoni speċifika dwar kull proċess ikun elenkat, inkluż proċess ID, running time, CPU u użu tal-memorja.';
+$lang['process_viewer_app_name'] = 'Viewer proċess';
+$lang['process_viewer_command'] = 'kmand';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'memorja';
+$lang['process_viewer_owner'] = 'sid';
+$lang['process_viewer_process_cpu_usage'] = 'Proċess Usage CPU';
+$lang['process_viewer_processes'] = 'proċessi';
+$lang['process_viewer_process_memory_usage'] = 'Użu Memorja Proċess';
+$lang['process_viewer_process'] = 'proċess';
+$lang['process_viewer_running'] = 'running';
+$lang['process_viewer_size'] = 'daqs';
