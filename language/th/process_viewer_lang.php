e176bd878f309fa05fe8a9703013b36012b524a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:07:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'ตัวแสดงกระบวนการให้แสดงตารางของกระบวนการทั้งหมดที่ทำงานบนระบบ ข้อมูลที่เฉพาะเจาะจงในแต่ละขั้นตอนการเป็น บริษัท จดทะเบียนรวมทั้งกระบวนการ ID, เวลาทำงาน, CPU และใช้หน่วยความจำ';
+$lang['process_viewer_app_name'] = 'Viewer กระบวนการ';
+$lang['process_viewer_command'] = 'คำสั่ง';
+$lang['process_viewer_cpu'] = 'ซีพียู';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'หน่วยความจำ';
+$lang['process_viewer_owner'] = 'เจ้าของ';
+$lang['process_viewer_process_cpu_usage'] = 'การใช้ CPU กระบวนการ';
+$lang['process_viewer_processes'] = 'กระบวนการ';
+$lang['process_viewer_process_memory_usage'] = 'ใช้หน่วยความจำในกระบวนการ';
+$lang['process_viewer_process'] = 'กระบวนการ';
+$lang['process_viewer_running'] = 'วิ่ง';
+$lang['process_viewer_size'] = 'ขนาด';
