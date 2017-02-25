0e806d2a1e0f0f113cf735232b998ecb35b968da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:57:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Prozesua ikuslea sisteman exekutatzen ari diren prozesu guztien erakusketa tabular bat eskaintzen du. prozesu bakoitzari buruzko informazio zehatza agertzen da, prozesu ID, denborak, CPU eta memoriaren erabilpena barne.';
+$lang['process_viewer_app_name'] = 'Prozesua Viewer';
+$lang['process_viewer_command'] = 'Komando';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Memoria';
+$lang['process_viewer_owner'] = 'Titularra';
+$lang['process_viewer_process_cpu_usage'] = 'Prozesua CPUaren erabilera';
+$lang['process_viewer_processes'] = 'Prozesuak';
+$lang['process_viewer_process_memory_usage'] = 'Prozesua Memoriaren erabilera';
+$lang['process_viewer_process'] = 'Prozesua';
+$lang['process_viewer_running'] = 'carrera';
+$lang['process_viewer_size'] = 'Tamaina';
