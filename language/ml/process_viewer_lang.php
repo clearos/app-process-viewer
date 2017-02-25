48cccb8865796920455a99916254252dfaf13728
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:03:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'പ്രോസസ്സ് വ്യൂവർ സിസ്റ്റത്തിൽ പ്രവർത്തിക്കുന്ന എല്ലാ പ്രക്രിയകളുടെ പട്ടികരൂപത്തിലുള്ള ഡിസ്പ്ലേ നൽകുന്നു. ഓരോ പ്രക്രിയയുടെ നിർദ്ദിഷ്ട വിവരങ്ങൾ സമയം, സിപിയു മെമ്മറി ഉപയോഗം പ്രവർത്തിക്കുന്ന, പ്രക്രിയ ഐഡി ഉൾപ്പെടെ പെടുത്തിയിട്ടുണ്ട്.';
+$lang['process_viewer_app_name'] = 'പ്രോസസ്സ് വ്യൂവർ';
+$lang['process_viewer_command'] = 'കമാൻഡ്';
+$lang['process_viewer_cpu'] = 'സിപിയു';
+$lang['process_viewer_id'] = 'ഐഡി';
+$lang['process_viewer_memory'] = 'മെമ്മറി';
+$lang['process_viewer_owner'] = 'ഉടമ';
+$lang['process_viewer_process_cpu_usage'] = 'പ്രോസസ്സ് സിപിയു ഉപയോഗം';
+$lang['process_viewer_processes'] = 'പ്രോസസുകൾ';
+$lang['process_viewer_process_memory_usage'] = 'മെമ്മറി ഉപയോഗം';
+$lang['process_viewer_process'] = 'പ്രോസസ്സ്';
+$lang['process_viewer_running'] = 'പ്രവർത്തിക്കുന്ന';
+$lang['process_viewer_size'] = 'വലുപ്പം';
