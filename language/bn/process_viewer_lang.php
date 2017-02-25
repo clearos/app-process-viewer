cea71d3ee97d1e12f7812bc5094fc1f0610e510b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:57:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'প্রক্রিয়া ভিউয়ার সিস্টেমের উপর চলমান সকল প্রক্রিয়ার একটি ট্যাবুলার প্রদর্শন উপলব্ধ. প্রতিটি প্রসেসের উপর নির্দিষ্ট তথ্য প্রসেস ID, চলমান সময়, CPU ও মেমরি ব্যবহার সহ তালিকাভুক্ত করা হয়.';
+$lang['process_viewer_app_name'] = 'প্রক্রিয়া ভিউয়ার';
+$lang['process_viewer_command'] = 'আদেশ';
+$lang['process_viewer_cpu'] = 'সিপিইউ';
+$lang['process_viewer_id'] = 'আইডি';
+$lang['process_viewer_memory'] = 'স্মৃতি';
+$lang['process_viewer_owner'] = 'মালিক';
+$lang['process_viewer_process_cpu_usage'] = 'প্রসেস CPU ব্যবহার';
+$lang['process_viewer_processes'] = 'প্রসেস';
+$lang['process_viewer_process_memory_usage'] = 'প্রসেস মেমরি ব্যবহারের মাত্রা';
+$lang['process_viewer_process'] = 'প্রক্রিয়া';
+$lang['process_viewer_running'] = 'চলমান';
+$lang['process_viewer_size'] = 'আয়তন';
