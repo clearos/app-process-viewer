e697149ec5bfd968d7f3541256eb76b84922a3a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:07:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Process Viewer sistem üzerinde çalışan tüm işlemlerin bir tablo görüntü sağlar. her işlem hakkındaki özel bilgiler işlem kimliği, çalışma süresi, CPU ve bellek kullanımı da dahil olmak üzere, listelenir.';
+$lang['process_viewer_app_name'] = 'Process Viewer';
+$lang['process_viewer_command'] = 'komuta';
+$lang['process_viewer_cpu'] = 'İşlemci';
+$lang['process_viewer_id'] = 'İD';
+$lang['process_viewer_memory'] = 'Bellek';
+$lang['process_viewer_owner'] = 'Sahip';
+$lang['process_viewer_process_cpu_usage'] = 'Süreç CPU Kullanımı';
+$lang['process_viewer_processes'] = 'süreçler';
+$lang['process_viewer_process_memory_usage'] = 'Süreç Bellek Kullanımı';
+$lang['process_viewer_process'] = 'süreç';
+$lang['process_viewer_running'] = 'Koşu';
+$lang['process_viewer_size'] = 'Boyut';
