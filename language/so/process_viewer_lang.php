b4401c764304cfa746010d8906870f71227cc636
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:05:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Daawade Hab-waxay bixisaa qaab tabular ah hababka oo dhan ordaya on nidaamka. warbixin gaar ah oo ku saabsan geedi socodka kasta waxaa ku qoran, oo ay ku jiraan geedi socodka ID, markii orodka, processor iyo isticmaalka xasuusta.';
+$lang['process_viewer_app_name'] = 'Hab-Daawade';
+$lang['process_viewer_command'] = 'Command';
+$lang['process_viewer_cpu'] = 'processor';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Xasuusta';
+$lang['process_viewer_owner'] = 'Mulkiilaha';
+$lang['process_viewer_process_cpu_usage'] = 'Hab-Isticmaalka processor';
+$lang['process_viewer_processes'] = 'Habsocodsiinta';
+$lang['process_viewer_process_memory_usage'] = 'Isticmaalka Xasuusta Hab-';
+$lang['process_viewer_process'] = 'Hab-';
+$lang['process_viewer_running'] = 'Running';
+$lang['process_viewer_size'] = 'size';
