24cfabdbef724e4a82bcfefa3cb5b0ff4f65e093
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:06:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Process Viewer ger en tabell visning av alla processer som körs på systemet. Specifik information om varje process är noterat, inklusive process-ID, gångtid, CPU och minnesanvändning.';
+$lang['process_viewer_app_name'] = 'process Viewer';
+$lang['process_viewer_command'] = 'Kommando';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Minne';
+$lang['process_viewer_owner'] = 'Ägare';
+$lang['process_viewer_process_cpu_usage'] = 'Process CPU-användning';
+$lang['process_viewer_processes'] = 'processer';
+$lang['process_viewer_process_memory_usage'] = 'Process Minnesanvändning';
+$lang['process_viewer_process'] = 'Bearbeta';
+$lang['process_viewer_running'] = 'Löpning';
+$lang['process_viewer_size'] = 'Storlek';
