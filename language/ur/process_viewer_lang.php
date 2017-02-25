6005597b234855b3981b6ec1f3ef195e243518ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:07:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'عمل ناظر سسٹم پر چلانے کے تمام عمل کی ایک ٹیبل ڈسپلے فراہم. ہر عمل پر مخصوص معلومات عمل ID، رننگ ٹائم، CPU اور میموری استعمال سمیت، درج کیا جاتا ہے.';
+$lang['process_viewer_app_name'] = 'عمل ناظر';
+$lang['process_viewer_command'] = 'کمانڈ';
+$lang['process_viewer_cpu'] = 'سی پی یو';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'یاداشت';
+$lang['process_viewer_owner'] = 'مالک';
+$lang['process_viewer_process_cpu_usage'] = 'عمل CPU استعمال';
+$lang['process_viewer_processes'] = 'عمل';
+$lang['process_viewer_process_memory_usage'] = 'عمل میموری استعمال';
+$lang['process_viewer_process'] = 'عمل';
+$lang['process_viewer_running'] = 'رننگ';
+$lang['process_viewer_size'] = 'سائز';
