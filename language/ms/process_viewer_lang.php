eb0765201e87ccb3c2cce7cd842a39a01e1a57da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:03:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'The Viewer Proses menyediakan paparan jadual untuk semua proses yang berjalan pada sistem. Maklumat khusus pada setiap proses disenaraikan, termasuk ID proses, masa berjalan, CPU dan penggunaan memori.';
+$lang['process_viewer_app_name'] = 'Viewer proses';
+$lang['process_viewer_command'] = 'Command';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'memori';
+$lang['process_viewer_owner'] = 'Owner';
+$lang['process_viewer_process_cpu_usage'] = 'Proses Penggunaan CPU';
+$lang['process_viewer_processes'] = 'proses';
+$lang['process_viewer_process_memory_usage'] = 'Penggunaan proses Memory';
+$lang['process_viewer_process'] = 'Proses';
+$lang['process_viewer_running'] = 'Running';
+$lang['process_viewer_size'] = 'saiz';
