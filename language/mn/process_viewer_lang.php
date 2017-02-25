1fbc2e92da9b4ecf02093aa7e9820d0b20570c6e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:03:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Процессийн Viewer систем дээр ажиллаж байгаа бүх үйл явц нь хүснэгтэн дэлгэцийг олгодог. үйл явц тус бүр дээр тодорхой мэдээлэл процесс ID, ажиллаж байгаа хугацаа, CPU болон санах ойн хэрэглээ зэрэг орсон байна.';
+$lang['process_viewer_app_name'] = 'үйл явц Viewer';
+$lang['process_viewer_command'] = 'тушаал';
+$lang['process_viewer_cpu'] = 'CPU-ийн';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Санах ойн';
+$lang['process_viewer_owner'] = 'Эзэмшигч';
+$lang['process_viewer_process_cpu_usage'] = 'Үйл явц нь CPU-ийн хэрэглээ';
+$lang['process_viewer_processes'] = 'Үйл явц';
+$lang['process_viewer_process_memory_usage'] = 'Үйл явц нь санах ойн хэрэглээ';
+$lang['process_viewer_process'] = 'үйл явц';
+$lang['process_viewer_running'] = 'ажиллаж байгаа';
+$lang['process_viewer_size'] = 'Хэмжээ';
