6d66e8f61153b31c9c0f9b7fb8b4dca88fe84b64
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:08:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Awọn ilana wiwo pese a tabular àpapọ gbogbo ilana nṣiṣẹ lori awọn eto. Kan pato alaye lori kọọkan ilana ti wa ni akojọ, pẹlu ilana ID, yen akoko, Sipiyu ati iranti lilo.';
+$lang['process_viewer_app_name'] = 'ilana wiwo';
+$lang['process_viewer_command'] = 'pipaṣẹ';
+$lang['process_viewer_cpu'] = 'Sipiyu';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'Memory';
+$lang['process_viewer_owner'] = 'eni';
+$lang['process_viewer_process_cpu_usage'] = 'Ilana Sipiyu Lilo';
+$lang['process_viewer_processes'] = 'lakọkọ';
+$lang['process_viewer_process_memory_usage'] = 'Ilana Memory Lilo';
+$lang['process_viewer_process'] = 'ilana';
+$lang['process_viewer_running'] = 'nṣiṣẹ';
+$lang['process_viewer_size'] = 'iwọn';
