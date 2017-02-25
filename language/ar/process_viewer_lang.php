50ba31aa405166dba187988fe5eab1d433c65b25
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:56:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'يوفر عارض عملية عرض جداول لجميع العمليات الجارية على النظام. يتم سرد معلومات محددة عن كل عملية، بما في ذلك معرف العملية، وقت التشغيل، وحدة المعالجة المركزية واستخدام الذاكرة.';
+$lang['process_viewer_app_name'] = 'عارض عملية';
+$lang['process_viewer_command'] = 'أمر';
+$lang['process_viewer_cpu'] = 'وحدة المعالجة المركزية';
+$lang['process_viewer_id'] = 'هوية شخصية';
+$lang['process_viewer_memory'] = 'ذاكرة';
+$lang['process_viewer_owner'] = 'صاحب';
+$lang['process_viewer_process_cpu_usage'] = 'عملية استخدام CPU';
+$lang['process_viewer_processes'] = 'العمليات';
+$lang['process_viewer_process_memory_usage'] = 'عملية استخدام الذاكرة';
+$lang['process_viewer_process'] = 'معالج';
+$lang['process_viewer_running'] = 'جري';
+$lang['process_viewer_size'] = 'بحجم';
