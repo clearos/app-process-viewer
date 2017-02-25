3350aa341723aff5b5bebe2a719918d9da0493ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:05:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Tsela mmogedi o fana ka tafola ponahatso ea dithulaganyo tsohle matha tsamaisong eo. lintlha tse hlakileng ka &#39;ngoe tiriso ya thulaganyo e thathamisitsoeng, ho akarelletsa tiriso ya thulaganyo ID, nako mathang, CPU le tšebeliso mohopolo oa motho.';
+$lang['process_viewer_app_name'] = 'Process mmogedi o';
+$lang['process_viewer_command'] = 'taelo';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'memori';
+$lang['process_viewer_owner'] = 'mong&#39;a';
+$lang['process_viewer_process_cpu_usage'] = 'Process CPU tšebeliso';
+$lang['process_viewer_processes'] = 'dithulaganyo';
+$lang['process_viewer_process_memory_usage'] = 'Process Krrish tšebeliso';
+$lang['process_viewer_process'] = 'Process';
+$lang['process_viewer_running'] = 'matha';
+$lang['process_viewer_size'] = 'Size';
