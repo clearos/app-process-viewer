2a647e8729ac85f52c7df3d4ce6ff97f59b086d4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:04:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Procesul Viewer oferă o afișare tabelară a tuturor proceselor care rulează pe sistem. Informații specifice privind fiecare proces este listat, inclusiv ID-ul de proces, timpul de funcționare, CPU și utilizarea memoriei.';
+$lang['process_viewer_app_name'] = 'Procesul Viewer';
+$lang['process_viewer_command'] = 'Comanda';
+$lang['process_viewer_cpu'] = 'Procesor';
+$lang['process_viewer_id'] = 'ID-ul';
+$lang['process_viewer_memory'] = 'Memorie';
+$lang['process_viewer_owner'] = 'Proprietar';
+$lang['process_viewer_process_cpu_usage'] = 'Procesul Utilizare CPU';
+$lang['process_viewer_processes'] = 'procese';
+$lang['process_viewer_process_memory_usage'] = 'Utilizarea proceselor de memorie';
+$lang['process_viewer_process'] = 'Proces';
+$lang['process_viewer_running'] = 'Alergare';
+$lang['process_viewer_size'] = 'mărimea';
