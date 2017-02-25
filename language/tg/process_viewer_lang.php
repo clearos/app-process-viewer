4146fb3a2206ac2388e4b77ecad04f578f4ef067
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:06:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Дар Навид Раванди Намоиши tabular ҳамаи равандҳои иҷро дар бораи системаи таъмин менамояд. маълумоти мушаххас дар бораи ҳар як раванди номбар мешавад, аз ҷумла, раванди ID, вақт давида, CPU ва истифодаи хотираи.';
+$lang['process_viewer_app_name'] = 'Раванди Навид';
+$lang['process_viewer_command'] = 'фармон';
+$lang['process_viewer_cpu'] = 'ВПМ - Воҳиди Пардозиши Марказӣ';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'хотира';
+$lang['process_viewer_owner'] = 'молик';
+$lang['process_viewer_process_cpu_usage'] = 'Раванди CPU Истифодабарӣ';
+$lang['process_viewer_processes'] = 'равандҳое,';
+$lang['process_viewer_process_memory_usage'] = 'Хотира раванд Истифодабарӣ';
+$lang['process_viewer_process'] = 'Раванди';
+$lang['process_viewer_running'] = 'якбора';
+$lang['process_viewer_size'] = 'андоза';
