864677dfabe6ac37d4ec24cb86e8c18af9fd01b2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 09:57:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'U Viewer proces daje tabelarni prikaz svih procesa koji rade na sistemu. Konkretne informacije o svakom procesu je navedeni, uključujući i proces ID, trčanje vremena, CPU i memorije.';
+$lang['process_viewer_app_name'] = 'proces Viewer';
+$lang['process_viewer_command'] = 'naredba';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'memorija';
+$lang['process_viewer_owner'] = 'vlasnik';
+$lang['process_viewer_process_cpu_usage'] = 'Proces CPU Usage';
+$lang['process_viewer_processes'] = 'procesi';
+$lang['process_viewer_process_memory_usage'] = 'Proces Memorijsko korištenje';
+$lang['process_viewer_process'] = 'proces';
+$lang['process_viewer_running'] = 'Running';
+$lang['process_viewer_size'] = 'Veličina';
