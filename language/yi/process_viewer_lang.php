958451d8f829cbe9cc48656921f661d22b4e2d93
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:08:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'דער פּראָצעס וויוער גיט אַ טאַבולאַר אַרויסווייַזן פון אַלע פּראַסעסאַז פליסנדיק אויף דעם סיסטעם. ספּעציפיש אינפֿאָרמאַציע אויף יעדער פּראָצעס איז ליסטעד, כולל פּראָצעס שייַן, פליסנדיק צייַט, קפּו און זיקאָרן באַניץ.';
+$lang['process_viewer_app_name'] = 'פּראָצעס וויוער';
+$lang['process_viewer_command'] = 'באַפעלן';
+$lang['process_viewer_cpu'] = 'קפּו';
+$lang['process_viewer_id'] = 'שייַן';
+$lang['process_viewer_memory'] = 'זיקאָרן';
+$lang['process_viewer_owner'] = 'באַזיצער';
+$lang['process_viewer_process_cpu_usage'] = 'פּראָצעס קפּו יוסידזש';
+$lang['process_viewer_processes'] = 'פּראָסעססעס';
+$lang['process_viewer_process_memory_usage'] = 'פּראָצעס זיקאָרן באַניץ';
+$lang['process_viewer_process'] = 'פּראָצעס';
+$lang['process_viewer_running'] = 'פליסנדיק';
+$lang['process_viewer_size'] = 'גרייס';
