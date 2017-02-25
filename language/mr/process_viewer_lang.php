4ca1a164edb3bbb0031fdd3601d962e5b5b4502a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:03:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'प्रक्रिया दर्शक प्रणालीवरील कार्यरत सर्व प्रक्रिया तक्त्याच्या प्रदर्शन उपलब्ध आहे. प्रत्येक प्रक्रिया विशिष्ट माहिती कार्यपध्दती ID रनिंग वेळ, CPU व मेमरी वापर, सूचीबद्ध आहे.';
+$lang['process_viewer_app_name'] = 'प्रक्रिया दर्शक';
+$lang['process_viewer_command'] = 'आदेश';
+$lang['process_viewer_cpu'] = 'सीपीयू';
+$lang['process_viewer_id'] = 'आयडी';
+$lang['process_viewer_memory'] = 'मेमरी';
+$lang['process_viewer_owner'] = 'मालक';
+$lang['process_viewer_process_cpu_usage'] = 'प्रक्रिया CPU वापर';
+$lang['process_viewer_processes'] = 'प्रक्रिया';
+$lang['process_viewer_process_memory_usage'] = 'प्रक्रिया मेमरी वापर';
+$lang['process_viewer_process'] = 'प्रक्रिया';
+$lang['process_viewer_running'] = 'कार्यरत';
+$lang['process_viewer_size'] = 'आकार';
