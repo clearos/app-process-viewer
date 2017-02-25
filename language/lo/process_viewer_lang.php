b29f58659f7f2fbdd0b09f90e18dfd149e0a43b1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:02:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'ການ Viewer ຂະບວນການສະຫນອງການສະແດງຕາຕະລາງຂອງຂະບວນການທັງຫມົດທີ່ແລ່ນໃນລະບົບ. ຂໍ້ມູນສະເພາະກ່ຽວກັບແຕ່ລະຂະບວນການຈົດທະບຽນ, ລວມທັງຂະບວນການ ID, ທີ່ໃຊ້ເວລາເຮັດວຽກ, CPU ແລະການນໍາໃຊ້ຄວາມຊົງຈໍາ.';
+$lang['process_viewer_app_name'] = 'Viewer ຂະບວນການ';
+$lang['process_viewer_command'] = 'ຄໍາສັ່ງ';
+$lang['process_viewer_cpu'] = 'CPU';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'ຫນ່ວຍຄວາມຈໍາ';
+$lang['process_viewer_owner'] = 'ເຈົ້າຂອງ';
+$lang['process_viewer_process_cpu_usage'] = 'ການນໍາໃຊ້ CPU ຂະບວນການ';
+$lang['process_viewer_processes'] = 'ຂະບວນການ';
+$lang['process_viewer_process_memory_usage'] = 'ການນໍາໃຊ້ຄວາມຈໍາຂະບວນການ';
+$lang['process_viewer_process'] = 'ຂະບວນການ';
+$lang['process_viewer_running'] = 'ເຮັດວຽກ';
+$lang['process_viewer_size'] = 'ຂະຫນາດ';
