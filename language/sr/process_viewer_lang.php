c63448e2101f0765f310fe2eeb518d808f7cb492
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:05:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Процес Виевер даје табеларни приказ свих процеса који раде на систему. Специфичне информације о сваком процесу наведен, укључујући и процес ИД, трајање, процесора и меморије.';
+$lang['process_viewer_app_name'] = 'proces Виевер';
+$lang['process_viewer_command'] = 'команда';
+$lang['process_viewer_cpu'] = 'Процесори';
+$lang['process_viewer_id'] = 'ИД';
+$lang['process_viewer_memory'] = 'Меморија';
+$lang['process_viewer_owner'] = 'Власник';
+$lang['process_viewer_process_cpu_usage'] = 'Процес ЦПУ Усаге';
+$lang['process_viewer_processes'] = 'procesi';
+$lang['process_viewer_process_memory_usage'] = 'Процес меморије Употреба';
+$lang['process_viewer_process'] = 'Процес';
+$lang['process_viewer_running'] = 'Руннинг';
+$lang['process_viewer_size'] = 'величина';
