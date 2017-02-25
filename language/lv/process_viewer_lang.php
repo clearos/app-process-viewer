17cc8eca1e955896322290bf766c0da4816aa65f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 10:02:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/process_viewer_lang.php
@@ -0,0 +1,15 @@
+<?php
+
+$lang['process_viewer_app_description'] = 'Process Viewer piedāvā tabulāro displejs visiem procesiem, kas darbojas sistēmā. Konkrēta informācija par katra procesa ir uzskaitīti, tostarp procesa ID, darba laika, CPU un atmiņas izmantošanu.';
+$lang['process_viewer_app_name'] = 'process Viewer';
+$lang['process_viewer_command'] = 'komanda';
+$lang['process_viewer_cpu'] = 'Procesors';
+$lang['process_viewer_id'] = 'ID';
+$lang['process_viewer_memory'] = 'atmiņa';
+$lang['process_viewer_owner'] = 'īpašnieks';
+$lang['process_viewer_process_cpu_usage'] = 'Process CPU Usage';
+$lang['process_viewer_processes'] = 'Procesi';
+$lang['process_viewer_process_memory_usage'] = 'Process Memory Usage';
+$lang['process_viewer_process'] = 'process';
+$lang['process_viewer_running'] = 'Running';
+$lang['process_viewer_size'] = 'Izmērs';
