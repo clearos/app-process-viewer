86fb0d9c4fce7ce328ce8308e8e4a65f445234c3
pcbaldwin <peter@egloo.ca>
2017-03-02 11:28:13 -0500

    Prepped for translations rebuild

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'process_viewer';
-$app['version'] = '2.3.20';
+$app['version'] = '2.3.22';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-process-viewer.spec
+++ packaging/app-process-viewer.spec
@@ -1,7 +1,7 @@
 
 Name: app-process-viewer
 Epoch: 1
-Version: 2.3.20
+Version: 2.3.22
 Release: 1%{dist}
 Summary: Process Viewer
 License: GPLv3
